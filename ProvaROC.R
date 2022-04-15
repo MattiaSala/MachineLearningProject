a85442ff86e789f8f6835e8e8d213b9c970ea468
Mattia <mattiasala09@gmail.com>
2022-04-15 18:02:13 +0200

    Add ROC example

---
+++ ProvaROC.R
@@ -0,0 +1,100 @@
+install.packages("c50")
+library(C50)
+install.packages("caret")
+library(caret)
+install.packages("ROCR")
+library(ROCR)
+install.packages("pROC ")
+library(pROC)
+
+
+data(churn)
+churnTrain = churnTrain[,! names(churnTrain) %in% c("state", "area_code", "account_length") ]
+set.seed(2)
+ind = sample(2, nrow(churnTrain), replace = TRUE, prob=c(0.7, 0.3))
+trainset = churnTrain[ind == 1,]
+testset = churnTrain[ind == 2,]
+
+
+#SVM
+svm.model= train(churn ~ ., data = trainset, method = "svmRadial")
+svm.pred = predict(svm.model, testset[,! names(testset) %in% c("churn")])
+table(svm.pred, testset[,c("churn")])
+
+result = confusionMatrix(svm.pred, testset[,c("churn")])
+result2 = confusionMatrix(svm.pred, testset[,c("churn")], mode = "prec_recall")
+
+
+#ROC CURVE
+svmfit=svm(churn~ ., data=trainset, prob=TRUE)
+pred=predict(svmfit,testset[, !names(testset) %in% c("churn")], probability=TRUE)
+pred.prob = attr(pred, "probabilities")
+pred.to.roc = pred.prob[, 2]
+pred.rocr = prediction(pred.to.roc, testset$churn)
+perf.rocr = performance(pred.rocr, measure = "auc", x.measure = "cutoff")
+perf.tpr.rocr = performance(pred.rocr, "tpr","fpr")
+
+plot(perf.tpr.rocr, colorize=T,main=paste("AUC:",(perf.rocr@y.values)))
+abline(a=0, b=1)
+
+
+#optimal cutoff
+opt.cut = function(perf, pred){
+  cut.ind = mapply(FUN=function(x, y, p){
+    d = (x - 0)^2 + (y-1)^2
+    ind = which(d == min(d))
+    c(sensitivity = y[[ind]], specificity = 1-x[[ind]],
+      cutoff = p[[ind]])
+  }, perf@x.values, perf@y.values, pred@cutoffs)
+}
+
+print(opt.cut(perf.tpr.rocr, pred.rocr))
+
+
+#overall accuracy
+acc.perf = performance(pred.rocr, measure = "acc")
+plot(acc.perf)
+
+
+#maximum accuracy
+ind = which.max( slot(acc.perf, "y.values")[[1]] )
+acc = slot(acc.perf, "y.values")[[1]][ind]
+cutoff = slot(acc.perf, "x.values")[[1]][ind]
+print(c(accuracy= acc, cutoff = cutoff))
+
+
+#comparing models
+control = trainControl(method = "repeatedcv", number = 10,repeats = 3,
+                       classProbs = TRUE, summaryFunction = twoClassSummary)
+
+svm.model= train(churn ~ ., data = trainset, method = "svmRadial", metric =
+                   "ROC", trControl = control)#train SVM
+
+rpart.model= train(churn ~ ., data = trainset, method = "rpart", metric = "ROC",
+                   trControl = control) #train DT
+
+svm.probs = predict(svm.model, testset[,! names(testset) %in% c("churn")],type = "prob")#predictions on SVM
+rpart.probs = predict(rpart.model, testset[,! names(testset) %in% c("churn")],type = "prob") #predictions on DT
+
+
+#generate ROC for both models and plot together
+svm.ROC = roc(response = testset[,c("churn")], predictor =svm.probs$yes,
+              levels = levels(testset[,c("churn")]))
+plot(svm.ROC,type="S", col="green")
+
+rpart.ROC = roc(response = testset[,c("churn")], predictor =rpart.probs$yes,
+                levels = levels(testset[,c("churn")]))
+plot(rpart.ROC, add=TRUE, col="blue")
+
+
+#compare AUC
+svm.ROC
+rpart.ROC
+
+#values
+cv.values = resamples(list(svm=svm.model, rpart = rpart.model))
+summary(cv.values)
+
+cv.values$timings #time to train
+
+
--- main.R
+++ main.R
@@ -10,10 +10,10 @@ install.packages("RColorBrewer")
 install.packages("varImp")
 install.packages("GGally")
 install.packages(c("FactoMineR", "factoextra")) #da slide
-install.packages(rpart)
-install.packages(rattle)
-install.packages(rpart.plot)
-install.packages(RColorBrewer)
+install.packages("rpart")
+install.packages("rattle")
+install.packages("rpart.plot")
+install.packages("RColorBrewer")
 
 
 library(dplyr)
@@ -27,8 +27,8 @@ library(corrplot)
 library(RColorBrewer)
 library(varImp)
 library(GGally)
-library(FactoMineR) #da slide
-library(factoextra) #da slide
+library(FactoMineR) 
+library(factoextra)
 library(rpart)
 library(rattle)
 library(rpart.plot)
@@ -54,17 +54,30 @@ water_potability <- water_potability %>%
 water_potability %>% summarise_all(~ sum(is.na(.)))
 
 #PCA
-pca.res <- PCA(water_potability[1:9], ncp=6, graph = TRUE)
+pca.res <- PCA(water_potability[1:9], graph = TRUE)
 
 #eigenvalues
 eig.val <- get_eigenvalue(pca.res)
 
 fviz_eig(pca.res, addlabels = TRUE, ylim = c(0, 20)) #scree plot
 
+# Contributions to the principal components
 var <- get_pca_var(pca.res)
-head(var$coord, 6)
+head(var$contrib)
+print(var$contrib)
+
 
 ind <- get_pca_ind(pca.res)
+fviz_pca_ind(pca.res)
+
+fviz_pca_ind(pca.res, col.ind = "cos2",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+                                repel = TRUE # Avoid text overlapping (slow if many points)
+             )
+
+name <- list(c("Solids","Sulfate", "ph"))
+fviz_pca_var(pca.res , select.var = list(cos2=3))
+
 
 #Decision Tree (DT)
 split.data = function(data, p = 0.7, s = 1){ #create function to split data
@@ -92,20 +105,99 @@ confusion.matrix = table(testset$Potability, testset$Prediction)
 #calculate accuracy
 sum(diag(confusion.matrix))/sum(confusion.matrix)
 
+#--------------------------------------------------------------------------------------------------------------
+
 #TODO - better model (incomplete):
+mean_hardness_ts <- mean(trainset$Hardness)
+var_hardness_ts <- var(trainset$Hardness)
+sd_hardness <- sd(trainset$Hardness)
 h0 <- subset(trainset, Potability==0) #subset of trainset where Potability = 0
 summary(h0$Hardness) #Look at the mean of Hardness where Potability = 0
 
 h1 <- subset(trainset, Potability==1) #subset of trainset where Potability = 1
 summary(h1$Hardness) #Look at the mean of Hardness where Potability = 1
 
+
+mean_sulfate_ts <- mean(trainset$Sulfate)
+var_sulfate_ts <- var(trainset$Sulfate)
+sd_sulfate <- sd(trainset$Sulfate)
 s0 <- subset(trainset, Potability==0) #subset of trainset where Potability = 0
 summary(s0$Sulfate) #Look at the mean of Sulfate where Potability = 0
 
 s1 <- subset(trainset, Potability==1) #subset of trainset where Potability = 1
 summary(s1$Sulfate) #Look at the mean of Sulfate where Potability = 1
 
-#train DT and plot it:
-decisionTree = rpart(Potability ~ Hardness + Sulfate, data=trainset, method="class")
-fancyRpartPlot(decisionTree)
+#ANALISI ESPLORATIVA DT
+#vedere in percentuale quanti elelemnti del trainset hanno valore > della media (di ciascuna variabile) se target == 0 | == 1
+barplot(
+  table(trainset$Potability, trainset$Sulfate), main="Sulfate",
+)
 
+hist(s0$Sulfate, main="Sulfate", xlab = "Sulfate")
+hist(s1$Sulfate, main="Sulfate", xlab = "Sulfate")
+
+s0_320_340 <- subset(s0, s0$Sulfate >= 320 & s0$Sulfate <= 340) #subset of trainset where Potability = 0
+hist(s0_320_340$Sulfate, main="Sulfate 320-340", xlab = "Sulfate")
+s1_300_350 <- subset(s1, s1$Sulfate >= 300 & s1$Sulfate <= 350) #subset of trainset where Potability = 0
+hist(s1_300_350$Sulfate, main="Sulfate 300-350", xlab = "Sulfate")
+
+
+sol_0 <- subset(trainset, Potability==0) #subset of trainset where Potability = 0
+summary(sol_0$Solids) #Look at the mean of Sulfate where Potability = 0
+
+sol_1 <- subset(trainset, Potability==1) #subset of trainset where Potability = 1
+summary(sol_1$Solids) #Look at the mean of Sulfate where Potability = 1
+
+barplot(
+  table(trainset$Potability, trainset$Solids), main="Solids",
+)
+
+hist(sol_0$Solids, main="Solids pota 0", xlab = "Solids")
+hist(sol_1$Solids, main="Solids pota 1", xlab = "Solids")
+
+sol_0_10k_30k <- subset(s0, sol_0$Solids >= 10000 & sol_0$Solids <= 30000) #subset of trainset where Potability = 0
+hist(sol_0_10k_30k$Solids, main="Solids 10k-30k", xlab = "Solids")
+
+#--------------------------------------------------------------------------------------------------------------
+
+#train DT and plot it:
+decisionTree = rpart(Potability ~ Sulfate + ph, data=trainset, method="class")
+fancyRpartPlot(decisionTree)
+printcp(decisionTree)
+plotcp(decisionTree)
+
+
+#accuracy with trained DT
+testset$Prediction <- predict(decisionTree, testset, type = "class")
+confusion.matrix = table(testset$Potability, testset$Prediction)
+sum(diag(confusion.matrix))/sum(confusion.matrix)
+
+#after prune of tree
+myPruned = prune(decisionTree, cp=.014)
+fancyRpartPlot(myPruned)
+
+testset$Prediction <- predict(myPruned, testset, type = "class")
+confusion.matrix = table(testset$Potability, testset$Prediction)
+sum(diag(confusion.matrix))/sum(confusion.matrix)
+
+#IG
+decisionTreeIG = rpart(Potability ~ Sulfate + ph, data=trainset, 
+                       method="class", 
+                       parms = list(split = 'information')
+                 )
+fancyRpartPlot(decisionTreeIG)
+printcp(decisionTreeIG)
+plotcp(decisionTreeIG)
+
+
+#accuracy with trained DTIG
+testset$Prediction <- predict(decisionTreeIG, testset, type = "class")
+confusion.matrix = table(testset$Potability, testset$Prediction)
+sum(diag(confusion.matrix))/sum(confusion.matrix)
+#after prune of tree
+myPruned = prune(decisionTreeIG, cp=.012)
+fancyRpartPlot(myPruned)
+
+testset$Prediction <- predict(myPruned, testset, type = "class")
+confusion.matrix = table(testset$Potability, testset$Prediction)
+sum(diag(confusion.matrix))/sum(confusion.matrix)
