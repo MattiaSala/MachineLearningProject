dfd4327cd314b45d2d4a8bbb8be66ddbb8b1fa34
acBicocca <104688810+acBicocca@users.noreply.github.com>
2022-04-30 19:37:17 +0200

    Update 10_fold_cross_validation_gradient_boosting.R

--- 10_fold_cross_validation_gradient_boosting.R
+++ 10_fold_cross_validation_gradient_boosting.R
@@ -1,4 +1,3 @@
-
 install.packages("irr")
 library(irr)
 set.seed(123)
@@ -6,13 +5,13 @@ folds <- createFolds(water_potability$Potability, k=2)
 results <- lapply(folds, function(x) {
   credit_train <- water_potability[-x, ]
   credit_test <- water_potability[x, ]
-  
   y_credit_train <- as.integer(credit_train$Potability) - 1 #togliamo 1 perchè xgb.DMatrix richiewde valori che partono da 0, as.integer restituisce valori che partono da 1
   y_credit_test <- as.integer(credit_test$Potability) - 1
+  complete_test_set <- credit_test
   
   credit_train <- credit_train %>% select(-Potability)
   credit_test <- credit_test %>% select(-Potability)
-  print(credit_train)
+  #print(credit_train)
   xgb_credit_train <- xgb.DMatrix(data = as.matrix(credit_train), label = y_credit_train)
   credit_model <- xgb.train(
     params = xgb_params,
@@ -21,14 +20,17 @@ results <- lapply(folds, function(x) {
     verbose = 1,
   )
   preds <- predict(credit_model, as.matrix(credit_test), reshape = TRUE)
-  #preds <- as.data.frame(preds)
-  return(data.frame(preds, real = water_potability$Potability[y_credit_test + 1]))
-  #credit_pred$PredictedClass <- apply(credit_pred, 1, function(y) colnames(credit_pred)[which.max(y)])
-  #credit_pred$ActualClass <- levels(water_potability$Potability)[y_credit_test + 1]
-  #kappa <- kappam.fleiss(data.frame(credit_actual, credit_pred))$value
-  #accuracy <- sum(credit_pred$PredictedClass == credit_pred$ActualClass) / nrow(credit_pred)
-  #return(accuracy)
+  preds <- as.data.frame(preds)
+  #return(data.frame(preds, real =  levels(water_potability$Potability)[y_credit_test + 1]))
+  colnames(preds) <- levels(water_potability$Potability)
+
+  preds$PredictedClass <- apply(preds, 1, function(y) colnames(preds)[which.max(y)])
+  preds$ActualClass <- complete_test_set$Potability
+  preds$PredictedClass <- as.factor(preds$PredictedClass)
+  preds$ActualClass <- as.factor(preds$ActualClass)
+  return(preds)
 })
 results
-#confusion matrix confusion.matrix = table(testset$Potability, testset$Prediction)
-confusionMatrix(results$preds, results$real)
+
+#confusion.matrix = table(testset$Potability, testset$Prediction)
+confusionMatrix(results$PredictedClass, results$ActualClass)
